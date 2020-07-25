695d99279dcbd94cce115f252eae37b9d8e616be
Omadoye Abraham <omadoye@trackstreet.com>
2020-07-25 21:17:21 +0100

    Updates

---
+++ precache-manifest.341a753e85a5508d03276d98ab9627b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "011e16cde12afdb9a0adc9f91ce24302",
+    "url": "/scriptures-rolodex/index.html"
+  },
+  {
+    "revision": "34fef78a1c88af215ac5",
+    "url": "/scriptures-rolodex/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58e12d4b7f3e9ee9d3ad",
+    "url": "/scriptures-rolodex/static/js/2.972916ff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scriptures-rolodex/static/js/2.972916ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34fef78a1c88af215ac5",
+    "url": "/scriptures-rolodex/static/js/main.ad7aca57.chunk.js"
+  },
+  {
+    "revision": "0ab85bd00496db0f3602",
+    "url": "/scriptures-rolodex/static/js/runtime-main.9bc5feaf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/scriptures-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
