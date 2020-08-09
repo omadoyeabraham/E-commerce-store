5dfc298acbeb9727cba26a883f4d0479d0a6a285
Omadoye Abraham <omadoye@trackstreet.com>
2020-08-09 01:09:20 +0100

    Removed redux logger middleware on production

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
-import { persistStore } from 'redux-persist'
+import { persistStore } from "redux-persist";
 
 import rootReducer from "./root.reducer";
 
@@ -9,7 +9,12 @@ import rootReducer from "./root.reducer";
  */
 
 // Middleware for the redux store
-const middlewares = [logger];
+const middlewares = [];
+
+if (["local", "development"].includes(process.env.NODE_ENV)) {
+  middlewares.push(logger);
+}
+
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
@@ -19,4 +24,4 @@ export const store = createStore(
 
 export const persistor = persistStore(store);
 
-export default { store, persistor }
+export default { store, persistor };
